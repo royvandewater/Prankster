c2c9a2385e3673de7341cd9b00599b7f32df2922
Integrum Pairing Station <dev+pairing+station@integrumtech.com>
2011-03-30 18:44:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prankster::Application.config.secret_token = 'eb81b67f6374742dc0c08860be41b713692950e1dbafa5e0b971dc026547996013a1b15e691b05a2a41b204907930a1ee2aeabb5ff72f2246175c6a9ad6b7f1c'
